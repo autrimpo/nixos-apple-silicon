d056d0e449f29439d511bd0ce27415dcae4d4f57
Michal Koutenský <michal@koutensky.net>
2025-03-21 15:52:39 +0100

    alsa-ucm-conf-asahi: 6 -> 7

--- apple-silicon-support/packages/alsa-ucm-conf-asahi/default.nix
+++ apple-silicon-support/packages/alsa-ucm-conf-asahi/default.nix
@@ -3,14 +3,14 @@
 , alsa-ucm-conf }:
 
 (alsa-ucm-conf.overrideAttrs (oldAttrs: rec {
-  version = "6";
+  version = "7";
 
   src_asahi = fetchFromGitHub {
     # tracking: https://src.fedoraproject.org/rpms/alsa-ucm-asahi
     owner = "AsahiLinux";
     repo = "alsa-ucm-conf-asahi";
     rev = "v${version}";
-    hash = "sha256-xTCGBohJz+VbHUvEoT3upwD8c+mSo2l7zncA1VIpZKg=";
+    hash = "sha256-CT2YIJoR7fUvDajYqO8LvbLo9S4C12CUCqGTnkiiS5o=";
   };
   
   postInstall = oldAttrs.postInstall or "" + ''
